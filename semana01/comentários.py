34835e4d15e041fcdb7aa46caa5e732dd907b529
Luciano dii Souza <luciano.souza@sistemafaculdade.com.br>
2020-03-25 16:05:19 -0300

    Backuping Mac - Python IFSP course

---
+++ semana01/comentários.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Isto é um comentário e não será executado pelo Python
+print("Oi de novo Python")
+print("Explicando comentários") # isto também é um comentário
---
+++ semana01/input.py
@@ -0,0 +1,2 @@
+nome = input('Digite aqui o seu nome: ')
+print('Ola, ' + nome)
